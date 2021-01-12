714c69ab70ca3ba6a0d87889f2ee8e386734c8ae
bardals <bardals@bitbucket.org>
2021-01-12 17:26:39 +0100

    2

--- controller.py
+++
@@ -1,8 +0,0 @@
-def application(environ, start_response):
-    # Genero la salida HTML a mostrar al usuario
-    output = "<p>Bienvenido a mi <b>PythonApp</b>!!!</p>"
-    #output = bytes(output, encoding='utf-8')
-    # Inicio una respuesta al navegador
-    start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
-    # Retorno el contenido HTML
-    return [bytes(output, 'utf-8')]
